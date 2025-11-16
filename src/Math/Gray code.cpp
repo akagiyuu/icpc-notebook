2303e3ff70fabaa9023f19b5a2bfc44f3a9ab141
akagiyuu <khanghuynh0245@gmail.com>
2025-11-16 22:57:20 +0700

    refactor: remove unneeded indentation

--- src/Math/Gray code.cpp
+++ src/Math/Gray code.cpp
@@ -1,6 +1,2 @@
-int g (int n) { return n ^ (n >> 1) }
-int rev_g (int g) {
-  int n = 0;
-  for (; g; g >>= 1) n ^= g;
-  return n;
-}
+int g(int n) { return n ^ (n >> 1); }
+int rev_g(int g) { int n = 0; for (; g; g >>= 1) n ^= g; }
