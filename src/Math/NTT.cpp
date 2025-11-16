5fb031be59e8a5771927fa2e4ec6e60c34df4772
akagiyuu <khanghuynh0245@gmail.com>
2025-11-16 22:53:07 +0700

    refactor(math): rename to NTT

--- src/Math/NTT.cpp
+++ src/Math/NTT.cpp
@@ -1,4 +1,4 @@
-const int MODULO = 998244353;
+const int MOD = 998244353;
 const int ROOT = 3; // Primitive root
 
 void fft(vector<int> &a, bool invert) {
@@ -7,25 +7,25 @@ void fft(vector<int> &a, bool invert) {
     int lg = __builtin_ctz(n);
     for (int i = 0; i < n; ++i) {
         int j = 0;
-        for (int k = 0; k < lg; ++k) if ((i&1<<k)!=0) j |= 1 << (lg-k-1);
+        for (int k = 0; k < lg; ++k) if ((i&1<<k)!=0) j |= 1ll << (lg-k-1);
         if (i < j) swap(a[i], a[j]);
     }
     for (int len = 2; len <= n; len *= 2) {
-        int wlen = power(ROOT, (MODULO - 1) / len);
+        int wlen = powmod(ROOT, (MOD - 1) / len);
         if (invert) wlen = inverse(wlen);
         for (int i = 0; i < n; i += len) {
             int w = 1;
             for (int j = 0; j < len / 2; ++j) {
                 int u = a[i + j];
-                int v = 1LL * a[i + j + len / 2] * w % MODULO;
-                a[i + j] = (u + v) % MODULO;
-                a[i + j + len / 2] = (u - v + MODULO) % MODULO;
-                w = 1LL * w * wlen % MODULO;
+                int v = a[i + j + len / 2] * w % MOD;
+                a[i + j] = add(u, v);
+                a[i + j + len / 2] = sub(u, v);
+                w = w * wlen % MOD;
             }
         }
     }
     if (invert) {
         int mul = inverse(n);
-        for (auto &x : a) x = 1LL * x * mul % MODULO;
+        for (auto &x : a) x = x * mul % MOD;
     }
 }
