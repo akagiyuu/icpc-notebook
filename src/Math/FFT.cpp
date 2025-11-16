8ff8ea9f2668f3fb5276a3d63d252889c5cd71e3
akagiyuu <khanghuynh0245@gmail.com>
2025-11-16 22:49:48 +0700

    feat(math): only keep 1 version of real number fft

--- src/Math/FFT.cpp
+++ src/Math/FFT.cpp
@@ -1,66 +1,59 @@
 typedef complex<double> Complex;
+const int LOG_FFT = 17;
+const double PI = acos(-1.0);
+static vector<Complex> fft_root(1 << LOG_FFT), inv_root(1 << LOG_FFT);
 
-template<class T> int size(const T &a) {
-    return a.size();
+void init_fft() {
+	int N = 1 << LOG_FFT;
+	for (int i = 0; i < N; ++i) {
+		double alpha = 2 * PI / N * i;
+		fft_root[i] = Complex(cos(alpha), sin(alpha));
+		inv_root[i] = Complex(cos(-alpha), sin(-alpha));
+	}
+}
+unsigned round_up(unsigned v) {
+	--v;
+	v |= v >> 1;
+	v |= v >> 2;
+	v |= v >> 4;
+	v |= v >> 8;
+	v |= v >> 16;
+	return v + 1;
 }
 
-unsigned roundUp(unsigned v) {
-    --v;
-    v |= v >> 1;
-    v |= v >> 2;
-    v |= v >> 4;
-    v |= v >> 8;
-    v |= v >> 16;
-    return v + 1;
-}
-
-int reverse(int num, int lg) {
-    int res = 0;
-    for(int i = 0; i < lg; ++i) if(num & 1 << i)
-        res |= 1 << (lg - i - 1);
-    return res;
-}
-
-template<class T> ostream& operator << (ostream& out, const vector<T> &a) {
-    for(int i = 0; i < size(a); ++i) {
-        if(i > 0) out << ' ';
-        out << a[i];
-    }
-    return out;
+int reverse_bits(int num, int lg) {
+	int res = 0;
+	for (int i = 0; i < lg; ++i)
+		if ((num >> i) & 1)
+			res |= 1 << (lg - i - 1);
+	return res;
 }
 
 vector<Complex> fft(vector<Complex> a, bool invert) {
-    int n = size(a), lg = 0;
-    while(1 << lg < n) ++lg;
-    vector<Complex> roots (n);
-    for(int i = 0; i < n; ++i) {
-        double alpha = 2 * M_PI / n * i * (invert ? -1 : 1);
-        roots[i] = Complex(cos(alpha), sin(alpha));
-    }
-    for(int i = 0; i < n; ++i) {
-        int rev = reverse(i, lg);
-        if(i < rev) swap(a[i], a[rev]);
-    }
-    for(int len = 2; len <= n; len <<= 1)
-        for(int i = 0; i < n; i += len)
-            for(int j = 0; j < len >> 1; ++j) {
-                Complex u = a[i + j], v = a[i + j + (len >> 1)] * roots[n / len * j];
-                a[i + j] = u + v;
-                a[i + j + (len >> 1)] = u - v;
-            }
-    if(invert) for(int i = 0; i < n; ++i) a[i] /= n;
-    return a;
-}
+	int n = (int)a.size();
+	int lg = 0;
+	while ((1 << lg) < n) ++lg;
 
-vector<long long> multiply(const vector<int> &a, const vector<int> &b) {
-    int n = roundUp(size(a) + size(b) - 1);
-    vector<Complex> pa (n), pb (n);
-    for(int i = 0; i < size(a); ++i) pa[i] = a[i];
-    for(int i = 0; i < size(b); ++i) pb[i] = b[i];
-    pa = fft(pa, false); pb = fft(pb, false);
-    for(int i = 0; i < n; ++i) pa[i] *= pb[i];
-    pa = fft(pa, true);
-    vector<long long> res (n);
-    for(int i = 0; i < n; ++i) res[i] = round(real(pa[i]));
-    return res;
+	int stride = (1 << LOG_FFT) / n;
+	vector<Complex> roots(n);
+	for (int i = 0; i < n; ++i)
+		roots[i] = invert ? inv_root[stride * i] : fft_root[stride * i];
+
+	for (int i = 0; i < n; ++i) {
+		int rev = reverse_bits(i, lg);
+		if (i < rev) swap(a[i], a[rev]);
+	}
+
+	for (int len = 2; len <= n; len <<= 1)
+		for (int i = 0; i < n; i += len)
+			for (int j = 0; j < (len >> 1); ++j) {
+				Complex u = a[i + j];
+				Complex v = a[i + j + (len >> 1)] * roots[n / len * j];
+				a[i + j] = u + v;
+				a[i + j + (len >> 1)] = u - v;
+			}
+
+	if (invert)
+		for (int i = 0; i < n; ++i) a[i] /= n;
+	return a;
 }
