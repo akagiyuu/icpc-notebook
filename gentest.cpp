a6c86e12266cf77b510aa697f6fb44970208013b
Thanh Triet Do <90243458+thanhtriet1007@users.noreply.github.com>
2025-12-04 13:25:08 +0700

    addSeedGentest

---
+++ gentest.cpp
@@ -0,0 +1,5 @@
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+long long get_rand(long long l, long long r) {
+    return uniform_int_distribution<long long>(l, r)(rng);
+}
