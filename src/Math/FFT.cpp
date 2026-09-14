5dee871e867f9e812f248da651b0e40480028499
akagiyuu <khanghuynh0245@gmail.com>
2026-09-14 10:25:58 +0700

    fix: make fft usable for many size

--- src/Math/FFT.cpp
+++ src/Math/FFT.cpp
@@ -1,12 +1,13 @@
 typedef complex<double> cd;
 const double PI = acos(-1.0);
+const int N = 1ll << 17;
 vector<cd> roots;
-void init_fft(int n) {
-	roots.resize(n >> 1);
-	const double angle = 2.0 * PI / n;
+void init_fft() {
+	roots.resize(N >> 1);
+	const double angle = 2.0 * PI / N;
 	const cd step(cos(angle), sin(angle));
 	roots[0] = cd(1.0, 0.0);
-	for (int i = 1; i < n / 2; ++i)
+	for (int i = 1; i < N / 2; ++i)
 		roots[i] = roots[i - 1] * step;
 }
 void fft(vector<cd> &a, bool invert) {
@@ -20,7 +21,7 @@ void fft(vector<cd> &a, bool invert) {
 		if (i < j) swap(a[i], a[j]);
 	}
 	for (int len = 2; len <= n; len <<= 1) {
-		const int step = n / len;
+		const int step = N / len;
 		const int half = len >> 1;
 		for (int i = 0; i < n; i += len) {
 			for (int j = 0; j < half; ++j) {
