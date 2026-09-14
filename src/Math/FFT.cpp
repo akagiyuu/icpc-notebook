2739d9e6e2d0d832a79eac1ed5567dffc4da2a55
akagiyuu <khanghuynh0245@gmail.com>
2026-09-14 09:32:42 +0700

    feat: optimize fft

--- src/Math/FFT.cpp
+++ src/Math/FFT.cpp
@@ -1,43 +1,39 @@
-typedef complex<double> Complex;
-const int LOG_FFT = 18;
+typedef complex<double> cd;
 const double PI = acos(-1.0);
-static vector<Complex> fft_root(1 << LOG_FFT), inv_root(1 << LOG_FFT);
- 
-void init_fft() {
-	int N = 1 << LOG_FFT;
-	for (int i = 0; i < N; ++i) {
-		double alpha = 2 * PI / N * i;
-		fft_root[i] = Complex(cos(alpha), sin(alpha));
-		inv_root[i] = Complex(cos(-alpha), sin(-alpha));
-	}
+vector<cd> roots;
+void init_fft(int n) {
+	roots.resize(n >> 1);
+	const double angle = 2.0 * PI / n;
+	const cd step(cos(angle), sin(angle));
+	roots[0] = cd(1.0, 0.0);
+	for (int i = 1; i < n / 2; ++i)
+		roots[i] = roots[i - 1] * step;
 }
-void fft(vector<Complex> &a, bool invert)
-{
-	int n = (int)a.size();
-	int lg = 0;
-	while ((1 << lg) < n) ++lg;
-	int stride = (1 << LOG_FFT) / n;
-	vector<Complex> roots(n);
-	for (int i = 0; i < n; ++i)
-		roots[i] = invert ? inv_root[stride * i] : fft_root[stride * i];
- 
+void fft(vector<cd> &a, bool invert) {
+	int n = a.size();
+	int lg = __builtin_ctz(n);
 	vector<int> rev(n);
 	for (int i = 1; i < n; ++i)
 		rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (lg - 1));
- 
-	for (int i = 0; i < n; ++i)
-		if (i < rev[i])
-			swap(a[i], a[rev[i]]);
- 
-	for (int len = 2; len <= n; len <<= 1)
-		for (int i = 0; i < n; i += len)
-			for (int j = 0; j < (len >> 1); ++j) {
-				Complex u = a[i + j];
-				Complex v = a[i + j + (len >> 1)] * roots[n / len * j];
+	for (int i = 0; i < n; ++i) {
+		int j = rev[i];
+		if (i < j) swap(a[i], a[j]);
+	}
+	for (int len = 2; len <= n; len <<= 1) {
+		const int step = n / len;
+		const int half = len >> 1;
+		for (int i = 0; i < n; i += len) {
+			for (int j = 0; j < half; ++j) {
+				cd w = roots[j * step];
+				if (invert) w = conj(w);
+				cd u = a[i + j];
+				cd v = a[i + j + half] * w;
 				a[i + j] = u + v;
-				a[i + j + (len >> 1)] = u - v;
+				a[i + j + half] = u - v;
 			}
+		}
+	}
 	if (invert)
-		for (int i = 0; i < n; ++i)
+		for (int i = 0; i < n; i++)
 			a[i] /= n;
 }
